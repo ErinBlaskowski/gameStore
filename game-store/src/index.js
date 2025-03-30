5fcc79a7438dc6a16d9004b341cdf29c28296603
ErinBlaskowski <blaskowe@email.sc.edu>
2025-03-30 15:31:27 -0400

    fix for BrowserReact issue not compiling

--- game-store/src/index.js
+++ game-store/src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import NoPage from "./pages/nopage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
