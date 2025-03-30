8416b80fcf031f85507882ef153c51b454b63446
ErinBlaskowski <blaskowe@email.sc.edu>
2025-03-30 18:09:42 -0400

    trying removing routes

--- game-store/src/index.js
+++ game-store/src/index.js
@@ -1,16 +1,10 @@
 import ReactDOM from "react-dom/client";
-// import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
