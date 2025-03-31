5c0a86423cc61493a32dc397d0c0242af26992f1
ErinBlaskowski <blaskowe@email.sc.edu>
2025-03-31 17:02:21 -0400

    added pages

--- game-store/src/index.js
+++ game-store/src/index.js
@@ -1,10 +1,21 @@
 import ReactDOM from "react-dom/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+import ItemPreview from "./pages/ItemPreview";
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="store" element={<Store />} />
+          <Route path="ItemPreview" element={<ItemPreview />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
