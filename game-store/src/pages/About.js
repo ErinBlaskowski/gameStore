535842f114d018014dd40c6c94d28d8b78886bde
ErinBlaskowski <blaskowe@email.sc.edu>
2025-03-31 22:33:16 -0400

    added all pages

--- game-store/src/pages/About.js
+++ game-store/src/pages/About.js
@@ -1,4 +1,6 @@
 import "./About.css";
+import crowsForWebsite from "../images/CrowsForWebsite.png";
+import meetTheTeam from "../images/MeetTheTeam.png";
 
 const About = () => {
     return (
@@ -26,6 +28,55 @@ const About = () => {
                 </ul>
             </nav>
             <h1 id="title">CrowPing Games</h1>
+            <main class="main">
+                <section class="flexbox box1">
+                    <img src={crowsForWebsite} height="650px" width="500px" alt="200x100"></img>
+                    <div class="text">
+                        <h3>What Is CrowPing Games?</h3>
+                        <p>CrowPing Games is not just an online gaming shop! We cater to those online and offline, with cyber deals, event hosting, gaming sessions, and a community game shop for all! Our physical store serves to connect customers into groups that can game together for years, catering especially to TTRPGs such as Dungeons & Dragons. Our digital store, on the other hand, provides fantastic prices for new and old gamers alike, catering to a broader audience with a wide variety of games!</p>
+                    </div>
+                </section>
+                <section class="flexbox box2">
+                    <img src={meetTheTeam} height="650px" width="500px" alt="200x100"></img>
+                    <div class="text">
+                        <h3>Meet The Team</h3>
+                        <p>Our talented staff includes the site manager, Erin Blaskowski, as well as our on-site employees, Lilith Hardwick and Martin Nguyen. Erin is an avid player of all board games, and Lilith and Erin play Dungeons & Dragons together at the shop from time to time, hosting events for those looking to play. Martin primarily manages inventory, but is always happy to discuss the best strategies for board games. </p>
+                    </div>
+                </section>
+        </main>
+
+        {/* iframe and form not implemented yet
+        <iframe id="iframe"
+            width = "560";
+            height = "315";
+            src = "https://www.youtube.com/embed/BgvHNlgmKro?si=sX8OzWESY8ovoN__";
+            allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            allowFullscreen = true; >
+        </iframe>
+        */}
+        <footer class="footer">
+            <div id="banner-menu">
+                <h2>Contact Us</h2>
+                <p>CrowPingGames@Outlook.com ~ 843-227-4592</p>
+            </div>
+        </footer>
+
+        {/*
+        </><form method="POST" id="form">
+            <input type="hidden" name="access_key" value="1ee4ff90-fa7d-4874-ab3c-1c5908dc72d9">
+
+            <input type="text" name="name" margin-top="5px" required>
+            <input type="email" name="email" margin-top="5px" required>
+            <textarea name="message" margin-top="5px" required></textarea>
+            <input type="checkbox" name="botcheck" class="hidden" margin-top="5px" style="display: none;">
+
+            <button type="submit">Submit Form</button>
+
+            <div id="result"></div>
+        </form>
+        <script src="form.js"></script>
+        </input>
+        */}
         </body>
         </>
     )
--- game-store/src/pages/Store.js
+++ game-store/src/pages/Store.js
@@ -1,5 +1,15 @@
 import "./Store.css";
 
+import monolopy from "../images/Monopoly.png";
+import diceSet from "../images/DiceSet.png";
+import dominos from "../images/Dominos.png";
+import funkyChessSet from "../images/FunkyChessSet.png";
+import medievalChessSet from "../images/MedievalChessSet.png";
+import virtualHeadset from "../images/VirtualHeadset.png";
+import redDiceSet from "../images/RedDiceSet.png";
+import bicycleCardSet from "../images/BicycleCardSet.png";
+import setOfEightPuzzles from "../images/SetOfEightPuzzles.jpeg";
+
 const Store = () => {
     return (
         <>
@@ -26,6 +36,55 @@ const Store = () => {
                 </ul>
             </nav>
             <h1 id="title">CrowPing Games</h1>
+            <main class="main">
+                <h1>THE BEST GAMES AT THE BEST PRICES!</h1>
+
+                <section class="columns">
+                    <div class="column1">
+                        <section id="box">
+                            <a href="./itemPreview"><img src={bicycleCardSet} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>Standard Card Set - $6.99</h3>
+                        </section>
+                        <section id="box">
+                            <a href="./itemPreview"><img src={diceSet} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>White Dice Set - $13.99</h3>
+                        </section>
+                        <section id="box">
+                            <a href="./itemPreview"><img src={dominos} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>Dominos - $15.99</h3>
+                        </section>
+                    </div>
+                    <div class="column2">
+                        <section id="box">
+                            <a href="./itemPreview"><img src={funkyChessSet} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>Layered Chess Set - $44.99</h3>
+                        </section>
+                        <section id="box">
+                            <a href="./itemPreview"><img src={medievalChessSet} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>Medieval Chess Set - $49.99</h3>
+                        </section>
+                        <section id="box">
+                            <a href="./itemPreview"><img src={redDiceSet} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>Red Dice Set - $13.99</h3>
+                        </section>
+                    </div>
+                    <div class="column3">
+                        <section id="box">
+                            <a href="./itemPreview"><img src={setOfEightPuzzles} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>Set of 8 Puzzles - $25.99</h3>
+                        </section>
+                        <section id="box">
+                            <a href="./itemPreview"><img src={virtualHeadset} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>VR Headset - $199.99</h3>
+                        </section>
+                        <section id="box">
+                            <a href="./itemPreview"><img src={monolopy} height="150px" width="200px" alt="200x50"></img></a>
+                            <h3>Monopoly - $24.99</h3>
+                        </section>
+                    </div>
+                </section>
+            </main>
+            {/* Not currently implemented: <script src="storeScript.js"></script> */}
         </body>
         </>
     )
